1c27eb487c3c186fb514b08e3242665543ebca79
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2020-05-01 14:39:43 +0200

    esercizio 04.1 completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { GEOJSON, GeoFeatureCollection } from './models/geojson.model';
-import {Marker} from './models/marker.model';
+import { GeoFeatureCollection } from './models/geojson.model';
+import { Marker } from './models/marker.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Ci_vettore } from './models/ci_vett.model';
 
 @Component({
   selector: 'app-root',
@@ -14,59 +17,60 @@ export class AppComponent implements OnInit {
   geoJsonObject: GeoFeatureCollection; //Oggetto che conterrà il vettore di GeoJson
   fillColor: string = "#FF0000";  //Colore delle zone catastali
   markers: Marker[];  //Vettore con tutti i marker
+  obsGeoData: Observable<GeoFeatureCollection>;
+  obsCiVett : Observable<Ci_vettore[]>;
+  lng: number = 9.205331366401035;
+  lat: number = 45.45227445505016;
 
-  lng: number = 9.02657833507687;
-  lat: number = 45.512051524024308;
-
-  constructor() {
-    //Questi dati dovremmo scaricarli dal server, per ora li abbiamo copiati nel file gojson.model.ts
-    this.geoJsonObject = GEOJSON;
-    console.log(this.geoJsonObject); //stampo l'oggetto geoJsonObject sulla console
-    //Provo a visualizzare le coordinate della prima features
-    console.log(this.geoJsonObject.features[0].geometry.coordinates);
+  constructor(public http: HttpClient) {
+  
   }
 
-  ngOnInit() {
-      //features[0] seleziona il primo geoJson
-      //coordinates[0] ottiene la lista di poligoni.
-      //coordinates[0][0] ottiene il primo (e unico) poligono della lista
-      //coordinates[0][0][0] ottiene la longitudine
-      //coordinates[0][0][1] ottiene la latitudine
-    /*this.markers = [
-      {
-        lng: this.geoJsonObject.features[0].geometry.coordinates[0][0][0],
-        lat: this.geoJsonObject.features[0].geometry.coordinates[0][0][1],
-        label: String(this.geoJsonObject.features[0].properties.id),
-      },
-      {
-        lng: this.geoJsonObject.features[1].geometry.coordinates[0][0][0],
-        lat: this.geoJsonObject.features[1].geometry.coordinates[0][0][1],
-        label: String(this.geoJsonObject.features[1].properties.id),
-      }
-    ]*/
+  prepareData = (data: GeoFeatureCollection) => {
+    this.geoJsonObject = data
+    
 
-    //Uso di un ciclo foreach per riempire i marker
-    this.markers = [];
+    console.log(this.geoJsonObject)
 
-    for (let feature of  this.geoJsonObject.features) {
+    /*for (let feature of this.geoJsonObject.features) {
       let lng = feature.geometry.coordinates[0][0][0];
       let lat = feature.geometry.coordinates[0][0][1];
       let id = String(this.geoJsonObject.features[0].properties.id);
-      let marker : Marker = new Marker(lat, lng, id);
+      let marker: Marker = new Marker(lat, lng, id);
       this.markers.push(marker);
-    }
+    }*/
+  }
 
+  prepareCiVettData = (data: Ci_vettore[]) =>
+  {
+    console.log(data);
+    this.markers = [];
+  
+    for (const iterator of data) {
+      let m = new Marker(iterator.WGS84_X,iterator.WGS84_Y,iterator.CI_VETTORE);
+      this.markers.push(m);
+    }
+    
+  }
+
+  ngOnInit() {
+    this.obsGeoData = this.http.get<GeoFeatureCollection>("http://localhost:3000");
+    this.obsGeoData.subscribe(this.prepareData);
+
+    this.obsCiVett = this.http.get<Ci_vettore[]>("http://localhost:3000/ci_vettore/90");
+    this.obsCiVett.subscribe(this.prepareCiVettData);
+    //Uso di un ciclo foreach per riempire i marker
   }
 
   styleFunc = (feature) => {
-    console.log(feature.i.id)
+    /*console.log(feature.i.id)
     let newColor = "#FF0000"; //RED
     if (feature.i.id == 0) newColor = "#00FF00"; //GREEN
-    else newColor = "#0000FF"; //BLUE
+    else newColor = "#0000FF"; //BLUE*/
 
     return ({
       clickable: false,
-      fillColor: newColor,
+      fillColor: this.fillColor,
       strokeWeight: 1
     });
   }
--- src/app/models/marker.model.ts
+++ src/app/models/marker.model.ts
@@ -1,6 +1,24 @@
 export class Marker {
-    constructor(
-        public lat: number,
-        public lng: number,
-        public label?: string) { }
+    icon = {}
+
+    constructor(public lat: number, public lng: number, public label?: string) 
+    {
+        if (this.label.includes("Gas")) {
+            this.icon = { url: './assets/img/gas.ico' };
+            this.label = "";
+        }
+        if(this.label.includes("elettrica"))
+        {
+            this.icon = { url: './assets/img/electricity.ico' };
+            this.label = "";
+        }
+        
+        
+    }
+
+
+   
+
+
+
 }
