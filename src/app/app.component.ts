9efd7fa7c0c0b9fd9016e04c1c38a6fcf667db0f
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2020-05-20 14:52:02 +0200

    progetto terminato

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,7 +28,10 @@ export class AppComponent implements OnInit {
   
   maxRadius: number = 400; //Voglio evitare raggi troppo grossi
 
+  loading: boolean = false;
   radius : number = this.maxRadius;
+
+  serverUrl : string = "http://localhost:3000"; //Proprietà per modificare una volta sola l'url del server
 
   constructor(public http: HttpClient) {
   
@@ -36,25 +39,16 @@ export class AppComponent implements OnInit {
 
   prepareData = (data: GeoFeatureCollection) => {
     this.geoJsonObject = data
-    console.log(this.geoJsonObject)
-
-    /*for (let feature of this.geoJsonObject.features) {
-      let lng = feature.geometry.coordinates[0][0][0];
-      let lat = feature.geometry.coordinates[0][0][1];
-      let id = String(this.geoJsonObject.features[0].properties.id);
-      let marker: Marker = new Marker(lat, lng, id);
-      this.markers.push(marker);
-    }*/
+    //console.log(this.geoJsonObject);
+    this.loading = false;
   }
 
   prepareCiVettData = (data: Ci_vettore[]) =>
   {
     let latTot = 0; //Uso queste due variabili per calcolare latitudine e longitudine media
     let lngTot = 0; //E centrare la mappa
-    
-    console.log(data);
+    //console.log(data);
     this.markers = [];
-    
     for (const iterator of data) {
       let m = new Marker(iterator.WGS84_X,iterator.WGS84_Y,iterator.CI_VETTORE);
       latTot += m.lat; //Sommo tutte le latitutidini e longitudini
@@ -67,14 +61,14 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.obsGeoData = this.http.get<GeoFeatureCollection>("http://localhost:3000");
-    this.obsGeoData.subscribe(this.prepareData);
+    //this.obsGeoData = this.http.get<GeoFeatureCollection>("http://localhost:3000");
+    //this.obsGeoData.subscribe(this.prepareData);
   }
 
   cambiaFoglio(foglio) : boolean
   {
     let val = foglio.value;
-    this.obsCiVett = this.http.get<Ci_vettore[]>(`http://localhost:3000/ci_vettore/${val}`);
+    this.obsCiVett = this.http.get<Ci_vettore[]>(`${this.serverUrl}/ci_vettore/${val}`);
     this.obsCiVett.subscribe(this.prepareCiVettData);
     console.log(val);
     return false;
@@ -110,34 +104,81 @@ export class AppComponent implements OnInit {
 
     let raggioInGradi = (this.radius * 0.00001)/1.1132;
 
+
+    const urlciVett = `${this.serverUrl}/ci_geovettore/
+    ${this.circleLat}/
+    ${this.circleLng}/
+    ${raggioInGradi}`;
+
+    const urlGeoGeom = `${this.serverUrl}/geogeom/
+    ${this.circleLat}/
+    ${this.circleLng}/
+    ${raggioInGradi}`;
     //Posso riusare lo stesso observable e lo stesso metodo di gestione del metodo cambiaFoglio
     //poichè riceverò lo stesso tipo di dati
     //Divido l'url andando a capo per questioni di leggibilità non perchè sia necessario
-    this.obsCiVett = this.http.get<Ci_vettore[]>(`http://localhost:3000/ci_geovettore/
-    ${this.circleLat}/
-    ${this.circleLng}/
-    ${raggioInGradi}`);
+    this.obsCiVett = this.http.get<Ci_vettore[]>(urlciVett);
     this.obsCiVett.subscribe(this.prepareCiVettData);
+
+    this.obsGeoData = this.http.get<GeoFeatureCollection>(urlGeoGeom);
+    this.obsGeoData.subscribe(this.prepareData);
 
     //console.log ("raggio in gradi " + (this.radius * 0.00001)/1.1132)
     
     //Voglio spedire al server una richiesta che mi ritorni tutte le abitazioni all'interno del cerchio
-
+    this.circleLat = 0;
+    this.circleLng = 0;
   }
 
- 
+  getAllData() : Boolean
+  {
+    this.loading = true;
+    //console.log("test");
+    this.obsGeoData = this.http.get<GeoFeatureCollection>(`${this.serverUrl}/allSez`);
+    this.obsGeoData.subscribe(this.prepareData);
+    return false;
+  }
 
   styleFunc = (feature) => {
-    /*console.log(feature.i.id)
-    let newColor = "#FF0000"; //RED
-    if (feature.i.id == 0) newColor = "#00FF00"; //GREEN
-    else newColor = "#0000FF"; //BLUE*/
-
     return ({
       clickable: false,
-      fillColor: this.fillColor,
-      strokeWeight: 1
+      fillColor: this.avgColorMap(feature.i.media),
+      strokeWeight: 1,
+      fillOpacity : 1  //Fill opacity 1 = opaco, fill opaticy 0 = trasparente 
     });
+  }
+
+
+  avgColorMap = (media) =>
+  {
+    if(media <= 36) return "#00FF00";
+    if(36 < media && media <= 40) return "#33ff00";
+    if(40 < media && media <= 58) return "#66ff00";
+    if(58 < media && media <= 70) return "#99ff00";
+    if(70 < media && media <= 84) return "#ccff00";
+    if(84 < media && media <= 100) return "#FFFF00";
+    if(100 < media && media <= 116) return "#FFCC00";
+    if(116 < media && media <= 1032) return "#ff9900";
+    if(1032 < media && media <= 1068) return "#ff6600";
+    if(1068 < media && media <= 1948) return "#FF3300";
+    if(1948 < media && media <= 3780) return "#FF0000";
+    return "#FF0000"
+  }
+
+  avgColorMapGreen = (media) =>
+  {
+    if(media <= 36) return "#EBECDF";
+    if(36 < media && media <= 40) return "#DADFC9";
+    if(40 < media && media <= 58) return "#C5D2B4";
+    if(58 < media && media <= 70) return "#ADC49F";
+    if(75 < media && media <= 84) return "#93B68B";
+    if(84 < media && media <= 100) return "#77A876";
+    if(100 < media && media <= 116) return "#629A6C";
+    if(116 < media && media <= 1032) return "#558869";
+    if(1032 < media && media <= 1068) return "#487563";
+    if(1068 < media && media <= 1948) return "#3B625B";
+    if(1948 < media && media <= 3780) return "#2F4E4F";
+    return "#003000" //Quasi nero
   }
   
 
--- src/app/models/marker.model.ts
+++ src/app/models/marker.model.ts
@@ -1,20 +1,73 @@
 import { Icon } from './icon.model';
+import { isNull } from 'util';
 
 export class Marker {
     icon : Icon;
     
     constructor(public lat: number, public lng: number, public label?: string) 
     {
+        if(isNull(this.label))
+        {
+            this.icon =  new Icon ('./assets/img/question-mark-32.ico' , 24);
+            this.label = "";
+            return;
+        }
+        if(this.label.includes("Gasolio"))
+        {
+            this.icon = new Icon ('./assets/img/warning-22-32.ico' , 24);
+            this.label = "";
+            return;
+        }
         if (this.label.includes("Gas")) {
             this.icon = new Icon ( './assets/img/gas.ico', 24 );
             this.label = "";
-            console.log(this.icon);
+            return;
+            //console.log(this.icon);
         }
         if(this.label.includes("elettrica"))
         {
             this.icon =  new Icon ( './assets/img/electricity.ico', 24 );
             this.label = "";
+            return;
         }
+        if(this.label.includes("solide"))
+        {
+            this.icon = new Icon ( './assets/img/tree-50-32.ico', 24);
+             this.label = "";
+             return;
+        }
+        if(this.label.includes("Olio"))
+        {
+            this.icon = new Icon (  './assets/img/water-32.ico', 24 );
+             this.label = "";
+             return;
+        }
+        if(this.label.includes("Teleriscaldamento"))
+        {
+            this.icon = new Icon ( './assets/img/power-32.ico', 24 );
+             this.label = "";
+             return;
+        }
+        if(this.label.includes("RSU"))
+        {
+            this.icon = new Icon ('./assets/img/trash-9-32.ico', 24);
+             this.label = "";
+             return;
+        }
+        if(this.label.includes("liquide"))
+        {
+            this.icon = new Icon ('./assets/img/toxic-32.ico', 24 );
+             this.label = "";
+             return;
+        }
+       
+        if(this.label.includes("GPL"))
+        {
+            this.icon = new Icon ('./assets/img/gas-pump-32.ico', 24 );
+             this.label = "";
+             return;
+        }
+        
     }
 
     changeIconSize(size)
